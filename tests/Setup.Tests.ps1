3ea853238f12262db769580df24cfadc97903daf
Michael K. Campbell <mike@overachiever.net>
2021-03-05 18:08:38 -0800

    Initial addition of tests.

---
+++ tests/Setup.Tests.ps1
@@ -0,0 +1,13 @@
+﻿
+$root = Split-Path -Parent $MyInvocation.MyCommand.Path.Replace("\tests", "\functions");
+$sut = Split-Path -Leaf $MyInvocation.MyCommand.Path.Replace(".Tests.", ".");
+$functionName = $sut.Replace(".ps1", "");
+
+
+Describe "Scaffolding Test" {
+	
+	It "Correctly Derives File Name" {
+		$functionName | Should -Be "Setup";
+	}
+	
+}

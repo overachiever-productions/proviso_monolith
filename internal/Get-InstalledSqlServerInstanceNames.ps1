18f1e62dcf16e9d7aa4e15b1574166f6132c8e31
Michael K. Campbell <mike@overachiever.net>
2021-03-23 08:58:50 -0700

    PRO10: Helper Function for perms assignment
    Allows for a) determination of which instances are installed and b) decoupling of those checks (to allow mocks/etc.)

---
+++ internal/Get-InstalledSqlServerInstanceNames.ps1
@@ -0,0 +1,6 @@
+﻿Set-StrictMode -Version 1.0;
+
+function Get-InstalledSqlServerInstanceNames {
+	[string[]]$output = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances;
+	return $output;
+}

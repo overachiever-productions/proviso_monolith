a8a512132fb419efdb8d7a74f5b29390a2f5f71d
Michael K. Campbell <mike@overachiever.net>
2021-03-23 14:04:05 -0700

    PRO11: Functionality + Tests to ensure shares
    Can be used for both IaC/setup of new shares and/or for processing ephemeral disks (i.e., disk X is gone, has been re-added at restart, and should have a share of Xyz on it with such and such perms).

---
+++ functions/Confirm-Shares.ps1
@@ -0,0 +1,59 @@
+﻿Set-StrictMode -Version 1.0;
+
+function Confirm-Shares {
+	
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	param (
+		[Parameter(Mandatory = $true)]
+		[PSCustomObject]$ServerDefinition,
+		[switch]$Strict = $true
+	);
+	
+	begin {
+		$currentHostName = $env:COMPUTERNAME;
+		if ($Strict) {
+			if ($currentHostName -ne $ServerDefinition.TargetServer) {
+				throw "HostName defined by $ServerDefinitionsPath [$($ServerDefinition.TargetServer)] does NOT match current server hostname [$currentHostName]. Processing Aborted."
+			}
+		}
+	}
+	
+	process {
+		$expectedShares = $ServerDefinition.ExpectedShares;
+		
+		# verify shares and perms:
+		[string[]]$shareKeys = $expectedShares.Keys;
+		foreach ($shareKey in $shareKeys) {
+			
+			$shareDefinition = $expectedShares.$shareKey;
+			[string]$shareName = $shareDefinition.ShareName;
+			[string]$shareDirectory = $shareDefinition.SourceDirectory;
+			
+			#create directory (if it doesn't exist):
+			Mount-Directory -Path $shareDirectory;
+			
+			# check to see if the share exists:			
+			$share = Get-SmbShare -Name $shareName -ErrorAction SilentlyContinue;
+			
+			if ($share -eq $null) {
+				New-SmbShare -Name $shareName -Path $shareDirectory;
+			}
+			
+			# now that it's created - either 'just now', or previously, ensure access (which is idempotent)
+			[string[]]$readAccess = $shareDefinition.ReadOnlyAccess;
+			[string[]]$fullAccess = $shareDefinition.ReadWriteAccess;
+			
+			foreach ($reader in $readAccess) {
+				Grant-SmbShareAccess -Name $shareName -AccountName $reader -AccessRight Read -Force;
+			}
+			
+			foreach ($account in $fullAccess){
+				Grant-SmbShareAccess -Name $shareName -AccountName $account -AccessRight Full -Force;
+			}
+		}
+	}
+	
+	end {
+		
+	}
+}

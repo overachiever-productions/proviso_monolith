d18aa97cd6f4a5b2543e0daa3b2490fa58548fe8
Michael K. Campbell <mike@overachiever.net>
2021-03-23 08:55:53 -0700

    PRO10: New Baseline func for existing volumes.

--- functions/Get-ExistingVolumeLetters.ps1
+++ functions/Get-ExistingVolumeLetters.ps1
@@ -1,9 +1,7 @@
 ﻿Set-StrictMode -Version 1.0;
 
-function Get-ExistingVolumesByLetter {
-	$drives = Get-Volume | Where-Object {
+function Get-ExistingVolumeLetters {
+	Get-Volume | Where-Object {
 		$_.DriveLetter -ne $null
 	} | Select-Object -ExpandProperty DriveLetter;
-	
-	return $drives;
 }

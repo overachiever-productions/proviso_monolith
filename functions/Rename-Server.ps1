d3ee2222c8f5009f52ae7a686ff50bbf55951cea
Michael K. Campbell <mike@overachiever.net>
2021-05-06 07:02:48 -0700

    Refactoring + making internal -> public

---
+++ functions/Rename-Server.ps1
@@ -0,0 +1,24 @@
+﻿Set-StrictMode -Version 1.0;
+
+function Rename-Server {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[string]$NewMachineName,
+		[switch]$AllowRestart = $true
+	);
+	
+	begin {
+		
+	};
+	
+	process {
+		
+		Rename-Computer -NewName $NewMachineName -Force -Restart:$AllowRestart;
+	};
+	
+	end {
+		
+	};
+}
--- internal/Get-InstalledSqlServerInstanceNames.ps1
+++
@@ -1,6 +0,0 @@
-﻿Set-StrictMode -Version 1.0;
-
-function Get-InstalledSqlServerInstanceNames {
-	[string[]]$output = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances;
-	return $output;
-}

a96377c919d76bc6e0b566fd76e87f6d8b147753
Michael K. Campbell <mike@overachiever.net>
2021-05-06 07:11:57 -0700

    workflow helper

---
+++ functions/Set-DefaultForOptionalValue.ps1
@@ -0,0 +1,15 @@
+﻿Set-StrictMode -Version 1.0;
+
+function Set-DefaultForOptionalValue {
+	param (
+		[string]$OptionalValue,
+		[Parameter(Mandatory = $true)]
+		[string]$DefaultValue
+	);
+	
+	if ([string]::IsNullOrEmpty($OptionalValue)) {
+		$OptionalValue = $DefaultValue;
+	}
+	
+	return $OptionalValue;
+}

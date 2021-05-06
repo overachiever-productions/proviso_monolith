3f99c72f8a63cacdc173362cb4031ef33d29f841
Michael K. Campbell <mike@overachiever.net>
2021-05-06 07:11:32 -0700

    Powershell 7+ Add-Computer (domain stuff)

--- functions/Install-ADManagementToolsForPowerShell6Plus.ps1
+++ functions/Install-ADManagementToolsForPowerShell6Plus.ps1
@@ -1,8 +1,14 @@
 ﻿Set-StrictMode -Version 1.0;
 
 function Install-ADManagementToolsForPowerShell6Plus {
-	# enables this 'module': https://docs.microsoft.com/en-us/powershell/module/addsadministration/?view=win10-ps
-	# which includes such hits and fantastical beats as New-AdComputer (replacement for Add-Computer), and Get-ADServiceAccount, etc. 
+	
+	# I ERRONEOUSLY thought that 'this was the way' to get Add-Computer back into play on Powershell 6+. It's not.
+	# BUT... there could be some really pertinent stuff here relative to CNO/VCO management and other AD-level stuff. 
+	
+	# Fodder: 
+	# 		- https://docs.microsoft.com/en-us/powershell/module/addsadministration/?view=win10-ps
+	# 		- https://docs.microsoft.com/en-us/powershell/module/activedirectory/?view=windowsserver2019-ps
+	# 		- http://woshub.com/powershell-active-directory-module/
 	
 	Install-WindowsFeature RSAT-AD-Powershell;
 }

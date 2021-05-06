963ccf83dbb53c5cef53249e64c73a062053c038
Michael K. Campbell <mike@overachiever.net>
2021-05-06 07:13:55 -0700

    Core Module Updates/Changes.

--- Proviso.psm1
+++ Proviso.psm1
@@ -1,27 +1,4 @@
-﻿<#
-
-	List Repositories: 
-		> Get-PSRepository
-
-
-	Registering a Repo (e.g., LabRepo): 
-		> Register-PSRepository -Name "LabRepo" -SourceLocation "\\storage.overachiever.net\Lab\ps-repository" -InstallationPolicy "Trusted";
-
-	Publication: 
-		- update the version if/as needed in the .psd1
-		- run the following: 
-			>  Publish-Module -Path "D:\Dropbox\Repositories\S4\S4 Tools\Proviso\" -Repository LabRepo                                   
-			(note that publication is by DIRECTORY - not by .psm1...)
-
-			Unpublish - in lab only = delete the .nupkg (i.e., there's no UnPublish command because you never know who/what (in the real world) might have taken a dependency on a module. 
-
-
-	Installation: 
-		> Install-Module -Name Proviso -Repository LabRepo
-
-		> Import-Module Proviso
-
-#>
+﻿
 
 $functionsPath = Join-Path -Path $PSScriptRoot -ChildPath 'functions/*.ps1';
 $internalFunctionsPath = Join-Path -Path $PSScriptRoot -ChildPath 'internal/*.ps1';

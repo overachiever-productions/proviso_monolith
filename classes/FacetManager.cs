35f4f6c9ab0d4ea87ded7af494845bef7820e344
Michael K. Campbell <mike@overachiever.net>
2021-09-27 08:17:47 -0700

    Refactor on name
    Catalog was probably a better name, but just sounds too odd/weird. Went with the pointless/non-descript 'Manager' instead.

---
+++ classes/FacetManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Proviso.Models
+{
+    public class FacetManager
+    {
+        // TODO: look at implementing this as an ORDERED dictionary;
+        private readonly Dictionary<string, Facet> _facets = new Dictionary<string, Facet>();
+        public int FacetCount
+        {
+            get { return this._facets.Count; }
+        }
+
+        private static readonly FacetManager _singletonInstance = new FacetManager();
+        static FacetManager() { }
+        private FacetManager() { }
+
+        public static FacetManager GetInstance()
+        {
+            return _singletonInstance;
+        }
+
+        //public static String GetStuff()
+        //{
+        //    return "this is just a simple test to validate that the stupid class is loading..";
+        //}
+
+        public string GetStuff()
+        {
+            return "it's hard to be dumb.";
+        }
+
+        public void AddFacet(Facet added)
+        {
+            this._facets.Add(added.Name, added);
+        }
+
+        //public void RemoveFacet(Facet removed)
+        //{
+
+        //}
+
+        public Facet GetFacet(string name)
+        {
+            if (this._facets.ContainsKey(name))
+                return this._facets[name];
+
+            return null;
+        }
+    }
+}

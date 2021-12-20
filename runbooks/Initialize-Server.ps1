0cee772614057e7bea0ee55c192ffcf9c923e56b
Michael K. Campbell <mike@overachiever.net>
2021-12-20 15:26:28 -0800

    miscellaneous 'ruminations' on runbooks
    nothing actual/tangible here... just rough ideas on possible approaches to syntax and other interactions/etc. with the DSL.

--- runbooks/Initialize-Server.ps1
+++ runbooks/Initialize-Server.ps1
@@ -1,18 +1,40 @@
 ﻿Set-StrictMode -Version 1.0;
 
-# Runbooks convention... they should be Verb-<something> ... so... "Ephemeral-Disks" sucks. It should be "Ensure-EphemeralDisks"
-#   well... then again... Runbook -For "(SQLServer-)High-Availability" makes plenty of sense... 
+<# 
+	Runbooks convention... they should be Verb-<something> ... so... "Ephemeral-Disks" sucks. It should be "Ensure-EphemeralDisks" or "Initialize-EphemeralDisks"
+	    well... then again... Runbook -For "(SQLServer-)High-Availability" makes plenty of sense... 
+	 
+	overall, the syntax below... works - i like it. 
+	   but... i THINK I'll probably have to look at ways to pass in a -MachineName as a required element in some of these?
+	   		e.g., InitializeServer... probably needs a -MachineName passed in. 
+	 				at which point, it'll determine if we're on the Host.TargetDomain or not... 
+	  							and, if needed, REQUIRE domainAdminCreds... 
 
-Runbook -Name "Initialize-Server" {
-	
+
+ 	In other words, I have to spend a bit more time figuring out what the 'caller' for some of these scripts might look like. Something like, say:
+
+	Assume something like this in either a .ps1 or ... from the command-line: 
+	> $SecurityThingy = Secured-By Something;
+	> $targetMachine = Func-To-Get-A-Name?
+	> Initialize-Server -MachineName $targetMachine -SecuredBy $securityThingy -UseConventionsForPathsAndStuff (or -usePersonalConfig@thislocationOrWhatever). 
+
+	> ... the end. 
+	I'd still need to bootstrap Proviso into play before running some of the stuff above... but... I'm going to need to PROVIDE: a) proviso, b) security-thingies, c) a machine-name (in many - but not all cases)
+
+
+#>
+
+Runbook -Name "Initialize-Server" -RequiresDomainCreds {
 	
 	With "xyz as config" | Secured-By $secureThingy | Invoke {
 		Configure-NetworkAdapters; # foreach [networkAdapter] in Host.NetworkDefinition... make sure we've got what we need.
 		Configure-IpAddress; # for each [networkAdapter]... ensure that the IP, subnet, and gateway are set as expected. 
 		Confgure-DNS; # foreach [networkAdapter]... ensure that DNS is set for primary/secondary as needed. 
 		
-		Configure-ServerName -AllowReboot; # - ExecuteRebase -Force;
+		Configure-ServerName -ExecuteRebase -Force;
 	}
+	
+	Summarize -All;
 	
 	if ($PVContext.RebootRequired) {
 		# figure out if ... there's a next Runbook... and set up a job to run that. 
@@ -32,7 +54,6 @@ Runbook -For "Prep-Host-For-SQLServer" {
 		
 		Configure-ExpectedDisks;
 		Configure-SqlDirectories;
-		
 	}
 }
 
@@ -71,7 +92,7 @@ Runbook -For "Install-SqlServer" {
 	}
 }
 
-Runbook -For "High-Availability" {
+Runbook -For "High-Availability" -RequiresDomainCreds {
 	
 }
 

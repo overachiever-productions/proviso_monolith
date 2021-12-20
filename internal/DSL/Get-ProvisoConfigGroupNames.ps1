a6d0524c1f3433e037167be0c8943c3ea7053fa0
Michael K. Campbell <mike@overachiever.net>
2021-12-20 15:25:38 -0800

    Helper for 'Group Definitions'
    Allows iteration over group names...

---
+++ internal/DSL/Get-ProvisoConfigGroupNames.ps1
@@ -0,0 +1,26 @@
+﻿Set-StrictMode -Version 1.0;
+
+function Get-ProvisoConfigGroupNames {
+	
+	param (
+		[Parameter(Mandatory)]
+		[PSCustomObject]$Config,
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$GroupKey
+		
+	);
+	
+	begin {
+		# do validations/etc. 
+	};
+	
+	process {
+		$block = Get-ProvisoConfigValueByKey -Config $Config -Key $GroupKey;
+		$keys = $block.Keys;
+	};
+	
+	end {
+		return $keys;
+	};
+}

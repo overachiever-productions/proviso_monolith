a2ec17392fa4870cf73d991ccf80ffa5bc101743
Michael K. Campbell <mike@overachiever.net>
2021-12-28 19:25:07 -0800

    Helper classes for core Facets/Definitions.

---
+++ internal/Install-NetFx35ForPre2016Instances.ps1
@@ -0,0 +1,21 @@
+﻿Set-StrictMode -Version 1.0;
+
+function Install-NetFx35ForPre2016Instances {
+	
+	param (
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateSet("Windows2012R2", "Windows2016", "Windows2019")]
+		[string]$WindowsServerVersion,
+		[Parameter(Mandatory)]
+		[string]$NetFxSxsRootPath
+	);
+	
+	$binariesPath = Join-Path -Path $NetFxSxsRootPath -ChildPath $WindowsServerVersion;
+	
+	$installed = (Get-WindowsFeature Net-Framework-Core).InstallState;
+	
+	if ($installed -ne "Installed") {
+		Install-WindowsFeature Net-Framework-Core -source $binariesPath
+	}
+}

cc33a36ecdd8c99bbb05cd0f2cb3a927aef5d4f4
Michael K. Campbell <mike@overachiever.net>
2021-12-28 19:26:12 -0800

    Code cleanup/formatting.
    removal of non-needed notes/comments.

--- functions/DSL/Definitions.ps1
+++ functions/DSL/Definitions.ps1
@@ -13,25 +13,6 @@ function Definitions {
 	& $Definitions;
 }
 
-<#
-
-	Import-Module -Name "D:\Dropbox\Repositories\proviso\" -DisableNameChecking -Force;
-
-	#With "\\storage\lab\proviso\definitions\servers\PRO\PRO-197.psd1" | Validate-FirewallRules;
-	
-	#With "\\storage\lab\proviso\definitions\servers\PRO\PRO-197.psd1" | Validate-RequiredPackages;
-
-	#With "\\storage\lab\proviso\definitions\servers\PRO\PRO-197.psd1" | Validate-LocalAdministrators;
-	#With "\\storage\lab\proviso\definitions\servers\PRO\PRO-197.psd1" | Validate-TestingFacet;
-	#With "\\storage\lab\proviso\definitions\servers\PRO\PRO-197.psd1" | Validate-DataCollectorSets;
-
-	#With "\\storage\lab\proviso\definitions\servers\PRO\PRO-197.psd1" | Configure-ServerName;
-	With "\\storage\lab\proviso\definitions\servers\PRO\PRO-197.psd1" | Validate-NetworkAdapters;
-
-	Summarize -All -IncludeAllValidations; # -IncludeAssertions;
-
-#>
-
 function Value-Definitions {
 	[Alias("KeyValue-Definitions")]
 	param (
@@ -47,21 +28,6 @@ function Value-Definitions {
 	
 	& $Definitions;
 }
-
-#region Notes
-# THESE NOTES USED TO BE In the LocalAdministrators Facet:
-# 		Finally... the above gets me that much closer to being able to handle things like: 
-# 			ExpectedShares - they're just a TINY bit more complex than "for N admins" - as in, they're multiple properties/values per each 'loop' or iteration. 
-# 		  ExpectedDirectories - ditto with the above. 
-# 		  EthernetAdapters - again, just multiple-ish properties. 
-# 		 	  ... then, getting more complex:
-# 		 	ExpectedDisks ... yeah, these are COMPLEX AF... but, 'each' is just a 'rule'... with a) lots of properties and b) COMPLEX state. 
-# 		    ... finally: 
-# 			SQL Installs, SQL Configs, and anything else that can/will be scoped by an {INSTANCE}
-# 				basically, they're just 'really complex disks or nics' in terms of complexity and scope/state. 
-# 					moreover, if i 'write' the code for MSSQLSERVER as an 'option' (plugable variable or whatever)
-# 					then... the ONLY thing that'd change for, say, a named instance... would be the name and so on... 
-#endregion  		
 
 function Group-Definitions {
 	[Alias("KeyGroup-Definitions")]

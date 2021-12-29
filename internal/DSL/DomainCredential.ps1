e6eaa71220dadb398fced22caa4f7a22c7c7a5af
Michael K. Campbell <mike@overachiever.net>
2021-12-28 19:23:49 -0800

    PRO156: Domain Creds for Assign Verb
    Initial stubs for how to handle domain creds...

---
+++ internal/DSL/DomainCredential.ps1
@@ -0,0 +1,15 @@
+﻿Set-StrictMode -Version 1.0;
+
+$global:PVDomainCreds = [Proviso.DomainCredential]::Instance;
+
+[ScriptBlock]$LoadCredential = {
+	param (
+		[Parameter(Mandatory)]
+		[string]$FilePath
+	);
+	
+	# do whatever it takes to load a cache object from disk or whatever... 
+	$savedCreds = "these are rehydrated from disk or whatever";
+	
+	$PVDomainCreds.AddCredential($savedCreds);
+}
---
+++ internal/Test-DomainCredentials.ps1
@@ -0,0 +1,21 @@
+﻿Set-StrictMode -Version 1.0;
+
+function Test-DomainCredentials {
+	param (
+		[Parameter(Mandatory)]
+		[PSCredential]$DomainCreds
+	);
+	
+	try {
+		$username = $DomainCreds.UserName;
+		$password = $DomainCreds.GetNetworkCredential().Password;
+		
+		$CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName;
+		$test = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain, $username, $password);
+		
+		return ($null -ne $test);
+	}
+	catch {
+		return $false;
+	}
+}

4eb70dc7a9b26e0b8f42227089afe30aa7a29300
Michael K. Campbell <mike@overachiever.net>
2022-02-17 18:24:06 -0800

    Additional windows-y consolidation of funcs

--- functions/internal/WindowsServer.ps1
+++ functions/internal/WindowsServer.ps1
@@ -77,7 +77,7 @@ function Get-WindowsServerVersion {
 	}
 }
 
-function Restart-Server {
+filter Restart-Server {
 	param (
 		[string]$RestartRunbookTarget = $null,
 		[int]$WaitSeconds = 0,
@@ -111,4 +111,67 @@ function Restart-Server {
 	else {
 		Write-Host "!!!!!!!!!!!!!!!!!!! 	SIMULATED REBOOT --- HAPPENING RIGHT NOW 	!!!!!!!!!!!!!!!!!!!";
 	}
+}
+
+filter Install-NetFx35ForPre2016Instances {
+	param (
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateSet("Windows2012R2", "Windows2016", "Windows2019")]
+		[string]$WindowsServerVersion,
+		[Parameter(Mandatory)]
+		[string]$NetFxSxsRootPath
+	);
+	
+	$binariesPath = Join-Path -Path $NetFxSxsRootPath -ChildPath $WindowsServerVersion;
+	
+	$installed = (Get-WindowsFeature Net-Framework-Core).InstallState;
+	
+	if ($installed -ne "Installed") {
+		Install-WindowsFeature Net-Framework-Core -source $binariesPath
+	}
+}
+
+filter Install-WsfcComponents {
+	# Fodder: https://docs.microsoft.com/en-us/powershell/module/failoverclusters/?view=windowsserver2019-ps
+	$rebootRequired = $false;
+	$processingError = $null;
+	
+	$installed = (Get-WindowsFeature -Name Failover-Clustering).InstallState;
+	switch ($installed) {
+		"Installed" {
+			$PVContext.WriteLog("WSFC Components already installed.", "Debug");
+		}
+		"InstallPending" {
+			$PVContext.WriteLog("Windows Feature 'Failover-Clustering' is in InstallPending state - i.e., installed by machine requires restart.", "Important");
+			$rebootRequired = $true;
+		}
+		"Available" {
+			try {
+				Install-WindowsFeature Failover-Clustering -IncludeManagementTools -ErrorVariable processingError | Out-Null;
+				
+				if ($null -ne $processingError) {
+					throw "Fatal error installing WSFC Components: $processingError ";
+				}
+			}
+			catch {
+				throw "Fatal Exception Encountered during installation of WSFC Components: $_ `r`t$($_.ScriptStackTrace)";
+			}
+			
+			if ($null -eq $processingError) {
+				$rebootRequired = $true;
+			}
+		}
+		default {
+			throw "WindowsFeature 'Failover-Clustering' is in an unexpected state: $installed. Terminating Proviso Execution.";
+		}
+	}
+	
+	$powershellInstalled = (Get-WindowsFeature -Name RSAT-Clustering-PowerShell).InstallState;
+	if ($powershellInstalled -ne "Installed") {
+		Install-WindowsFeature RSAT-Clustering-PowerShell -IncludeAllSubFeature | Out-Null;
+		$rebootRequired = $true;
+	}
+	
+	return $rebootRequired;
 }

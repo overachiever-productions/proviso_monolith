7a23adfae7a360463d6cdfac5636bc36bf6d03c3
Michael K. Campbell <mike@overachiever.net>
2022-04-12 09:55:17 -0700

    Code Cleanup/Refactoring.

--- internal/DataCollectorSets.ps1
+++ internal/DataCollectorSets.ps1
@@ -1,31 +1,5 @@
 ﻿Set-StrictMode -Version 1.0;
 
-filter Enable-DataCollectorSetForAutoStart {
-	param (
-		[Parameter(Mandatory = $true)]
-		[string]$Name,
-		[switch]$Disable
-	);
-	
-	if ($Disable) {
-		throw "Proviso Framework Error. Disabling DataCollectorSets for AutoStart (with OS) is not YET supported.";
-	}
-	
-	## assumes same convention used by Data Collector Setup - i.e., a task with the name of the data collector will be found in the \Microsoft\Windows\PLA\ folder. 
-	$task = Get-ScheduledTask -TaskName $Name -TaskPath "\Microsoft\Windows\PLA\";
-	$trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:00:05;
-	
-	if ((Get-WindowsServerVersion) -eq "Windows2019") {
-		## Implementation of work-around listed here: https://docs.microsoft.com/en-us/troubleshoot/windows-server/performance/user-defined-dcs-doesnt-run-as-scheduled
-		$newAction = New-ScheduledTaskAction -Execute "C:\windows\system32\rundll32.exe" -Argument "C:\windows\system32\pla.dll,PlaHost `"$Name`" `"`$(Arg0)`"";
-		
-		Set-ScheduledTask -TaskName $Name -TaskPath "\Microsoft\Windows\PLA\" -Action $newAction -Trigger $trigger | Out-Null;
-	}
-	else {
-		Set-ScheduledTask -TaskName $Name -TaskPath "\Microsoft\Windows\PLA\" -Trigger $trigger | Out-Null;
-	}
-}
-
 filter Get-DataCollectorSetStatus {
 	param (
 		[string]$Name
@@ -60,26 +34,6 @@ filter Get-DataCollectorSetStatus {
 	return $state;
 }
 
-filter New-DataCollectorSetFileCleanupJob {
-	param (
-		[string]$Name,
-		[int]$RetentionDays
-	);
-	
-	$jobName = "$Name - Cleanup Older Files";
-	$task = Get-ScheduledTask -TaskName $jobName -ErrorAction SilentlyContinue;
-	
-	if ($task -eq $null) {
-		$trigger = New-ScheduledTaskTrigger -At 2am -Daily;
-		$runAsUser = "NT AUTHORITY\SYSTEM";
-		$settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Minutes 5);
-		
-		$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy BYPASS -NonInteractive -NoProfile -File C:\PerfLogs\Remove-OldCollectorSetFiles.ps1 -Name $Name -RetentionDays $RetentionDays ";
-		$task = Register-ScheduledTask -TaskName $jobName -Trigger $trigger -Action $action -User $runAsUser -Settings $settings -RunLevel Highest -Description "Regular cleanup of Data Collector Set files (> 45 days old) for `"$Name`" Data Collecctor.";
-	}
-	
-}
-
 filter New-DataCollectorSetFromConfigFile {
 	param (
 		[string]$Name,
@@ -100,4 +54,72 @@ filter New-DataCollectorSetFromConfigFile {
 	
 	# force a wait before attempting start: 
 	Start-Sleep -Milliseconds 1800 | Invoke-Expression "logman.exe start `"$Name`"" | Out-Null;
+}
+
+filter Enable-DataCollectorSetForAutoStart {
+	param (
+		[Parameter(Mandatory)]
+		[string]$Name,
+		[switch]$Disable
+	);
+	
+	if ($Disable) {
+		throw "Proviso Framework Error. Disabling DataCollectorSets for AutoStart (with OS) is not YET supported.";
+	}
+	
+	## assumes same convention used by Data Collector Setup - i.e., a task with the name of the data collector will be found in the \Microsoft\Windows\PLA\ folder. 
+	$task = Get-ScheduledTask -TaskName $Name -TaskPath "\Microsoft\Windows\PLA\";
+	$trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:00:05;
+	
+	if ((Get-WindowsServerVersion) -eq "Windows2019") {
+		## Implementation of work-around listed here: https://docs.microsoft.com/en-us/troubleshoot/windows-server/performance/user-defined-dcs-doesnt-run-as-scheduled
+		$newAction = New-ScheduledTaskAction -Execute "C:\windows\system32\rundll32.exe" -Argument "C:\windows\system32\pla.dll,PlaHost `"$Name`" `"`$(Arg0)`"";
+		
+		Set-ScheduledTask -TaskName $Name -TaskPath "\Microsoft\Windows\PLA\" -Action $newAction -Trigger $trigger | Out-Null;
+	}
+	else {
+		Set-ScheduledTask -TaskName $Name -TaskPath "\Microsoft\Windows\PLA\" -Trigger $trigger | Out-Null;
+	}
+}
+
+filter New-DataCollectorSetFileCleanupJob {
+	param (
+		[Parameter(Mandatory)]
+		[string]$Name,
+		[Parameter(Mandatory)]
+		[int]$RetentionDays
+	);
+	
+	$jobName = "$Name - Cleanup Older Files";
+	$task = Get-ScheduledTask -TaskName $jobName -ErrorAction SilentlyContinue;
+	
+	if ($task -eq $null) {
+		$trigger = New-ScheduledTaskTrigger -At 2am -Daily;
+		$runAsUser = "NT AUTHORITY\SYSTEM";
+		$settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Minutes 5);
+		
+		$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy BYPASS -NonInteractive -NoProfile -File C:\PerfLogs\Remove-OldCollectorSetFiles.ps1 -Name $Name -RetentionDays $RetentionDays ";
+		$task = Register-ScheduledTask -TaskName $jobName -Trigger $trigger -Action $action -User $runAsUser -Settings $settings -RunLevel Highest -Description "Regular cleanup of Data Collector Set files (> 45 days old) for `"$Name`" Data Collecctor.";
+	}
+	
+}
+
+# TODO: turn this into a FUNC (not filter), allow it to be called per MULTIPLE data-collector sets 
+# 		e.g., 
+# 				> $targets = @("Consolidated","Mirroring","BatchStats", "Etc");
+# 				> Remove-OldDataCollectorFiles $DataCollectorName $targets 45;
+#		THEN, set up 'New-DataCollectorSetFileCleanupJob' to allow 1 or more targets ... i.e., create if NO targets, CHANGE if already 1 target/etc. 
+# 		in short: don't have N cleanup jobs ... have 1 instead.
+filter Remove-OldDataCollectorFiles {
+	param (
+		[Parameter(Mandatory)]
+		[string]$DataCollectorName,
+		[int]$DaysWorthOfLogsToKeep = 45,
+		[string]$RootFilePath = "C:\PerfLogs\"
+	);
+	
+	$threshold = (Get-Date).AddDays(0 - $DaysWorthOfLogsToKeep);
+	$directory = Join-Path -Path $RootFilePath -ChildPath $DataCollectorName;
+	
+	Get-ChildItem $directory | Where-Object CreationTime -lt $threshold | Remove-Item -Force;
 }

89ed9dcdd844ccc647db89de6373bf441e23db8d
Michael K. Campbell <mike@overachiever.net>
2022-04-12 09:54:55 -0700

    Refactor/Overhaul of code
    Kind of a goofy approach to defining 'member funcs' but makes code-collapse easier in Sapien PowerShell Studio ... so, meh.

--- internal/dsl/ProvisoResources.ps1
+++ internal/dsl/ProvisoResources.ps1
@@ -27,12 +27,7 @@
 
 #>
 
-[PSCustomObject]$global:PVResources = [PSCustomObject]@{
-	RootSet = $false
-	ProvisoRoot = $null
-};
-
-[ScriptBlock]$SetRoot = {
+filter PVResources-SetRoot {
 	param (
 		[Parameter(Mandatory)]
 		[string]$RootPath
@@ -53,13 +48,13 @@
 	$PVResources.ProvisoRoot = $RootPath;
 }
 
-[ScriptBlock]$ValidateRootSet = {
+filter PVResources-ValidateRootSet {
 	if (-not ($PVResources.RootSet)) {
 		throw "Configuration Error. Access to Proviso Resources can not be determined before `$PVResources.SetRoot() has been called.";
 	}
 }
 
-[ScriptBlock]$GetAsset = {
+filter PVResources-GetAsset {
 	param (
 		[Parameter(Mandatory)]
 		[string]$Asset,
@@ -118,7 +113,30 @@
 	}
 }
 
-[ScriptBlock]$GetSqlSetupExe = {
+filter PVResources-GetXeSessionDefinitionFile {
+	param (
+		[Parameter(Mandatory)]
+		[string]$DefinitionFile
+	);
+	
+	# allow for hard-coded paths: 
+	if (Test-Path -Path $DefinitionFile -ErrorAction SilentlyContinue) {
+		return $DefinitionFile;
+	}
+	
+	# otherwise, build/validate a path by convention: 
+	$PVResources.ValidateRootSet();
+	$provisoRoot = $PVResources.ProvisoRoot;
+	
+	$testPath = Join-Path -Path $provisoRoot -ChildPath "\assets\extended_events\$($DefinitionFile)";
+	if (Test-Path -Path $testPath -ErrorAction SilentlyContinue) {
+		return $testPath;
+	}
+	
+	return $null;
+}
+
+filter PVResources-GetSqlSetupExe {
 	param (
 		[Parameter(Mandatory)]
 		[string]$SetupKey
@@ -144,12 +162,12 @@
 	return $null;
 }
 
-[ScriptBlock]$GetSqlSetupIso = {
+filter PVResources-GetSqlSetupIso {
 	throw "Not Implemented.";
 	# NOT sure if this'll do the work to just FIND the iso, or find it, MOUNT IT, and then return a path to, say, U:\setup.exe or whatever... 
 }
 
-[ScriptBlock]$GetSsmsBinaries = {
+filter PVResources-GetSsmsBinaries {
 	param (
 		[Parameter(Mandatory)]
 		$BinaryKey
@@ -178,7 +196,7 @@
 	return $null;
 }
 
-[ScriptBlock]$GetSQLSpOrCu = {
+filter PVResources-GetSqlSpOrCu {
 	$PVResources.ValidateRootSet();
 	$provisoRoot = $PVResources.ProvisoRoot;
 	
@@ -192,10 +210,10 @@
 	#  ..\binaries\sqlserver\sps
 	
 	# i.e., i need to figure out the best OPTION(s) from above and then implement as needed. 
-
+	
 }
 
-[ScriptBlock]$GetServerDefinition = {
+filter PVResources-GetServerDefinition {
 	param (
 		[Parameter(Mandatory)]
 		[string]$HostName
@@ -204,7 +222,7 @@
 	# TODO: Implement - but the idea is that ... i can just specify a host name and this'll get an associated config.
 }
 
-[ScriptBlock]$GetAdminDbPath = {
+filter PVResources-GetAdminDbPath {
 	param (
 		[Parameter(Mandatory)]
 		[string]$InstanceName,
@@ -250,14 +268,240 @@
 	return $adminDbPath;
 }
 
-$PVResources | Add-Member -MemberType ScriptMethod -Name ValidateRootSet -Value $ValidateRootSet;
-$PVResources | Add-Member -MemberType ScriptMethod -Name SetRoot -Value $SetRoot;
-$PVResources | Add-Member -MemberType ScriptMethod -Name GetAsset -Value $GetAsset;
-$PVResources | Add-Member -MemberType ScriptMethod -Name GetSqlSetupExe -Value $GetSqlSetupExe;
-$PVResources | Add-Member -MemberType ScriptMethod -Name GetSqlSetupIso -Value $GetSqlSetupIso;
-$PVResources | Add-Member -MemberType ScriptMethod -Name GetSsmsBinaries -Value $GetSsmsBinaries;
-$PVResources | Add-Member -MemberType ScriptMethod -Name GetSqlSpOrCu -Value $GetSQLSpOrCu;
-$PVResources | Add-Member -MemberType ScriptMethod -Name GetAdminDbPath -Value $GetAdminDbPath;
+[PSCustomObject]$global:PVResources = [PSCustomObject]@{
+	RootSet = $false
+	ProvisoRoot = $null
+};
+
+#region PRevious
+#[ScriptBlock]$SetRoot = {
+#	param (
+#		[Parameter(Mandatory)]
+#		[string]$RootPath
+#	);
+#	
+#	if (-not (Test-Path $RootPath)) {
+#		throw "Invalid Path defined/assigned to `$PVResources via SetRoot() method.";
+#	}
+#	
+#	# vNEXT: Personal Config settings can/will allow these to be overridden. 
+#	foreach ($dir in "assets", "binaries", "definitions", "repository") {
+#		if (-not (Test-Path -Path (Join-Path -Path $RootPath -ChildPath $dir))) {
+#			$PVContext.WriteLog("Proviso Directory [$dir] not found in [$RootPath].", "Important");
+#		}
+#	}
+#	
+#	$PVResources.RootSet = $true;
+#	$PVResources.ProvisoRoot = $RootPath;
+#}
+
+#[ScriptBlock]$ValidateRootSet = {
+#	if (-not ($PVResources.RootSet)) {
+#		throw "Configuration Error. Access to Proviso Resources can not be determined before `$PVResources.SetRoot() has been called.";
+#	}
+#}
+
+#[ScriptBlock]$GetAsset = {
+#	param (
+#		[Parameter(Mandatory)]
+#		[string]$Asset,
+#		[string[]]$AllowedExtensions,
+#		[switch]$AllowPartialMatches = $false,
+#		[switch]$CaseSensitive = $true
+#	);
+#	
+#	$PVResources.ValidateRootSet();
+#	$provisoRoot = $PVResources.ProvisoRoot;
+#	$matchedPath = $null;
+#	
+#	# Asset may be an absolute path - in which case it's an override - and we're done:
+#	if (Test-Path -Path $Asset -ErrorAction SilentlyContinue) {
+#		$matchedPath = $Asset;
+#	}
+#	
+#	if ($null -eq $matchedPath) {
+#		
+#		foreach ($ext in $AllowedExtensions) {
+#			if (-not ($ext.StartsWith("."))) {
+#				$ext = ".$($ext)";
+#			}
+#			
+#			$testPath = Join-Path -Path $provisoRoot -ChildPath "\assets\$($Asset)$($ext)";
+#			if (Test-Path -Path $testPath) {
+#				$matchedPath = $testPath;
+#				break;
+#			}
+#		}
+#		
+#		if ($AllowPartialMatches) {
+#			$testPath = Join-Path -Path $provisoRoot -ChildPath "\assets\";
+#			$matches = Get-ChildItem -Path $testPath -Filter "*$($Asset)*";
+#			
+#			if ($matches.Count -eq 1) {
+#				$matchedPath = $matches[0].FullName;
+#			}
+#			if ($matches.Count -gt 1) {
+#				throw "Error. Attempt to use -PartialMatches for Asset [$Asset] against assets directory resulted in > 1 potential pattern match. Terminating...";
+#			}
+#		}
+#	}
+#	
+#	if ($matchedPath -ne $null) {
+#		if ($CaseSensitive) {
+#			$parent = Split-Path -Path $matchedPath;
+#			$child = Split-Path -Path $matchedPath -Leaf;
+#			
+#			$object = Get-ChildItem -Path $parent -Filter $child;
+#			return $object.FullName;
+#		}
+#		else {
+#			return $Asset;
+#		}
+#	}
+#}
+
+#[ScriptBlock]$GetSqlSetupExe = {
+#	param (
+#		[Parameter(Mandatory)]
+#		[string]$SetupKey
+#	);
+#	
+#	$PVResources.ValidateRootSet();
+#	$provisoRoot = $PVResources.ProvisoRoot;
+#	
+#	# Allow for hard-coded paths as the 'key' - i.e., overrides of convention can be specified... (e.g., Z:\setup.exe, etc.)
+#	if (Test-Path -Path $SetupKey) {
+#		return $SetupKey;
+#	}
+#	
+#	$SetupKey = Join-Path "binaries\sqlserver" -ChildPath $SetupKey;
+#	
+#	# TODO: convert this to use Join-Path from Posh 7 - where there's an -AdditionalChildPath switch... 
+#	[string]$path = Join-Path -Path $provisoRoot -ChildPath $SetupKey
+#	$path = Join-Path -Path $path -ChildPath "setup.exe";
+#	if (Test-Path -Path $path) {
+#		return $path;
+#	}
+#	
+#	return $null;
+#}
+
+#[ScriptBlock]$GetSqlSetupIso = {
+#	throw "Not Implemented.";
+#	# NOT sure if this'll do the work to just FIND the iso, or find it, MOUNT IT, and then return a path to, say, U:\setup.exe or whatever... 
+#}
+
+#[ScriptBlock]$GetSsmsBinaries = {
+#	param (
+#		[Parameter(Mandatory)]
+#		$BinaryKey
+#	);
+#	
+#	$PVResources.ValidateRootSet();
+#	$provisoRoot = $PVResources.ProvisoRoot;
+#	
+#	# Allow for hard-coded paths as the 'key' - i.e., overrides of convention can be specified... (e.g., Z:\setup.exe, etc.)
+#	if (Test-Path -Path $BinaryKey) {
+#		return $BinaryKey;
+#	}
+#	
+#	$BinaryKey = Join-Path "binaries\ssms" -ChildPath $BinaryKey;
+#	
+#	[string]$path = Join-Path -Path $provisoRoot -ChildPath "$($BinaryKey).exe";
+#	if (Test-Path -Path $path) {
+#		return $path;
+#	}
+#	
+#	[string]$path = Join-Path -Path $provisoRoot -ChildPath "$BinaryKey";
+#	if (Test-Path -Path $path) {
+#		return $path;
+#	}
+#	
+#	return $null;
+#}
+
+#[ScriptBlock]$GetSQLSpOrCu = {
+#	$PVResources.ValidateRootSet();
+#	$provisoRoot = $PVResources.ProvisoRoot;
+#	
+#	throw "Not Implemented.";
+#	# need to figure out what kind of folder structure to use here... i.e., it COULD be any of the following: 
+#	#  ..\binaries\patches
+#	#  ..\binaries\patches\cus
+#	#  ..\binaries\patches\sps
+#	#  ..\binaries\sqlserver\patches
+#	#  ..\binaries\sqlserver\cus
+#	#  ..\binaries\sqlserver\sps
+#	
+#	# i.e., i need to figure out the best OPTION(s) from above and then implement as needed. 
+#	
+#}
+
+#[ScriptBlock]$GetServerDefinition = {
+#	param (
+#		[Parameter(Mandatory)]
+#		[string]$HostName
+#	);
+#	
+#	# TODO: Implement - but the idea is that ... i can just specify a host name and this'll get an associated config.
+#}
+
+#[ScriptBlock]$GetAdminDbPath = {
+#	param (
+#		[Parameter(Mandatory)]
+#		[string]$InstanceName,
+#		[string]$OverridePath = $null
+#	);
+#	
+#	$adminDbPath = $null;
+#	
+#	if (-not ([string]::IsNullOrEmpty($OverridePath))) {
+#		if (Test-Path -Path $OverridePath) {
+#			$adminDbPath = $OverridePath;
+#		}
+#		else {
+#			throw "Invalid Server Configuration. Value [OverrideSource] for AdminDb.$InstanceName is invalid. Leave empty or specify a FULL path to admindb_latest.sql";
+#		}
+#	}
+#	else {
+#		$filePath = "C:\Scripts";
+#		Mount-Directory $filePath;
+#		
+#		$release = Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/overachiever-productions/S4/releases/latest" -TimeoutSec 12 -ErrorAction SilentlyContinue;
+#		if ($release) {
+#			$file = ($release.assets | Where-Object {
+#					$_.name -like "*.sql"
+#				})[0].browser_download_url;
+#			
+#			$outFile = $filePath | Join-Path -ChildPath "admindb_latest.sql";
+#			Invoke-WebRequest -Method GET -Uri $file -OutFile $outFile;
+#			
+#			$adminDbPath = $outFile;
+#		}
+#		# otherwise, assume we don't have network connectivity... 
+#	}
+#	
+#	if ($null -eq $adminDbPath) {
+#		$adminDbPath = $this.GetAsset("admindb_latest", "sql");
+#	}
+#	
+#	if ($null -eq $adminDbPath) {
+#		throw "Proviso Framework Error. Unable to locate an override, or default (local) copy of admindb_latest.sql - and could NOT download admindb from github.com.";
+#	}
+#	
+#	return $adminDbPath;
+#}
+#endregion
+
+$PVResources | Add-Member -MemberType ScriptMethod -Name SetRoot -Value ((Get-Item "Function:\PVResources-SetRoot").ScriptBlock) -Force;
+$PVResources | Add-Member -MemberType ScriptMethod -Name ValidateRootSet -Value ((Get-Item "Function:\PVResources-ValidateRootSet").ScriptBlock) -Force;
+$PVResources | Add-Member -MemberType ScriptMethod -Name GetAsset -Value ((Get-Item "Function:\PVResources-GetAsset").ScriptBlock) -Force;
+$PVResources | Add-Member -MemberType ScriptMethod -Name GetXeSessionDefinitionFile -Value ((Get-Item "Function:\PVResources-GetXeSessionDefinitionFile").ScriptBlock) -Force;
+$PVResources | Add-Member -MemberType ScriptMethod -Name GetSqlSetupExe -Value ((Get-Item "Function:\PVResources-GetSqlSetupExe").ScriptBlock) -Force;
+$PVResources | Add-Member -MemberType ScriptMethod -Name GetSqlSetupIso -Value ((Get-Item "Function:\PVResources-GetSqlSetupIso").ScriptBlock) -Force;
+$PVResources | Add-Member -MemberType ScriptMethod -Name GetSsmsBinaries -Value ((Get-Item "Function:\PVResources-GetSsmsBinaries").ScriptBlock) -Force;
+$PVResources | Add-Member -MemberType ScriptMethod -Name GetSqlSpOrCu -Value ((Get-Item "Function:\PVResources-GetSqlSpOrCu").ScriptBlock) -Force;
+$PVResources | Add-Member -MemberType ScriptMethod -Name GetAdminDbPath -Value ((Get-Item "Function:\PVResources-GetAdminDbPath").ScriptBlock) -Force;
 
 <#
 

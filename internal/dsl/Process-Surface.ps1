0a4cd3e781767d64d02d5c0ab68513d08091ad67
Michael K. Campbell <mike@overachiever.net>
2022-05-10 17:56:07 -0700

    Move Compare-ExpectedWithActual to Process-Surface

--- internal/dsl/Process-Surface.ps1
+++ internal/dsl/Process-Surface.ps1
@@ -118,6 +118,74 @@ filter New-DynamicFacet {
 	return $newFacet;
 }
 
+function Compare-ExpectedWithActual {
+	param (
+		$Expected,
+		# This is actually NULLable...
+		[Parameter(Mandatory)]
+		[ScriptBlock]$TestBlock
+	);
+	
+	begin {
+		
+	};
+	
+	process {
+		
+		#region vNEXT
+		# vNEXT: Look into using Compare-Object. 
+		# 		it's super powerful/complex  ... but, might make sense to verify that 'all' details/outputs were == 
+		# 		and potentially capture those that don't ==.
+		#endregion	
+		
+		$actualResult = $null;
+		$actualException = $null;
+		try {
+			$actualResult = & $TestBlock;
+		}
+		catch {
+			$actualException = $_;
+		}
+		
+		[bool]$comparedValuesMatch = $false;
+		
+		[System.Management.Automation.ErrorRecord]$comparisonError = $null;
+		if ($null -eq $actualException) {
+			
+			try {
+				if ($Expected -is [bool]) {
+					if ($Expected) {
+						# sigh. have to flip this around, cuz if(<somethingThatEvaluatesToTrue> -etc) is evaluated, we'll 100% short circuit if the data types are the exact same/etc. 
+						$comparedValuesMatch = ($actualResult -eq $Expected);
+					}
+					else {
+						if ((-not ($Expected)) -and (-not ($actualResult))) {
+							$comparedValuesMatch = $true; # Honestly, this is super lame... 
+						}
+					}
+				}
+				else {
+					$comparedValuesMatch = ($Expected -eq $actualResult);
+				}
+			}
+			catch {
+				$comparisonError = $_;
+			}
+		}
+	};
+	
+	end {
+		$output = [PSCustomObject]@{
+			'ActualResult'    = $actualResult
+			'ActualError'	  = $actualException
+			'Matched'		  = $comparedValuesMatch
+			'ComparisonError' = $comparisonError
+		};
+		
+		return $output;
+	};
+}
+
 function Process-Surface {
 	param (
 		[Parameter(Mandatory)]
